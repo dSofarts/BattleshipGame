a7ed233db418a276df167459a4e6e223cc725000
Egor <DronovEgorVl@yandex.com>
2021-06-21 14:47:56 +0300

    Creation of battlegrounds

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Game {
+    static final int FIELD_LENGTH = 10;
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Player 1, please write your name");
         String playerOneName = scanner.nextLine();
@@ -10,5 +12,11 @@ public class Game {
         System.out.println("Player 2, please write your name");
         String playerTwoName = scanner.nextLine();
         System.out.println("Hello, " + playerTwoName + "!");
+
+        char[][] playerFieldOne = new char[FIELD_LENGTH][FIELD_LENGTH];
+        char[][] playerFieldTwo = new char[FIELD_LENGTH][FIELD_LENGTH];
+
+        char[][] playerBattleOne = new char[FIELD_LENGTH][FIELD_LENGTH];
+        char[][] playerBattleTwo = new char[FIELD_LENGTH][FIELD_LENGTH];
     }
 }
