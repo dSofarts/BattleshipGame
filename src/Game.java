b4468ec83f366f74f9190f3a997046a6b93130db
Egor <DronovEgorVl@yandex.com>
2021-06-21 14:41:20 +0300

    Player initialization

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,14 @@
-public class Game {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Game {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Player 1, please write your name");
+        String playerOneName = scanner.nextLine();
+        System.out.println("Hello, " + playerOneName + "!");
+
+        System.out.println("Player 2, please write your name");
+        String playerTwoName = scanner.nextLine();
+        System.out.println("Hello, " + playerTwoName + "!");
     }
 }
